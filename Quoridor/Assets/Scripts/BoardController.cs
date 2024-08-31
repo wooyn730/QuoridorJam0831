1fb8066e12e1a7363b21bf480521c2dd1932129a
우연 <bethelight730@gmail.com>
2024-08-31 14:43:54 +0900

    이동 가능한 칸 활성화

---
+++ Quoridor/Assets/Scripts/BoardController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardController : MonoBehaviour
+{
+    [SerializeField] private Transform _board;
+    private Transform[,] _squares = new Transform[9, 9];
+    [SerializeField] private Vector2 player1Pos;
+    [SerializeField] private Vector2 player2Pos;
+    [SerializeField] private Vector2[] around = new Vector2[4];
+
+    // 보드의 9*9 정보를 가지고 있는다
+    // 플레이어의 위치를 확인한다
+    // 플레이어가 이동 가능한 보드의 색을 바꿔준다
+    // 누르면 플레이어가 해당 위치로 이동한다
+
+    private void Start()
+    {
+        InitAround();
+        AllocateSquares();
+        player1Pos = new Vector2(4, 8);
+        player2Pos = new Vector2(4, 0);
+        Player1Turn();
+    }
+
+    private void Player1Turn()
+    {
+        ActiveSquares(player1Pos);
+    }
+
+    private void ActiveSquares(Vector2 playerPos)
+    {
+        // 갈 수 있는 칸 활성화
+        for (int i=0; i<around.Length; i++)
+        {
+            int newX = (int)(playerPos.x + around[i].x);
+            int newY = (int)(playerPos.y + around[i].y);
+
+            if (newX >= 0 && newX < 9 && newY >= 0 && newY < 9)
+            {
+                Image image = _squares[newY, newX].GetComponent<Image>();
+                image.color = new Color(255/255f, 244/255f, 143/255f);
+            }
+        }
+    }
+
+    private void AllocateSquares()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                // Debug.Log(_board.GetChild(i * 9 + j));
+                _squares[i, j] = _board.GetChild(i * 9 + j);
+            }
+        }
+    }
+
+    private void InitAround()
+    {
+        around[0] = new Vector2(-1, 0);
+        around[1] = new Vector2(1, 0);
+        around[2] = new Vector2(0, -1);
+        around[3] = new Vector2(0, 1);
+    }
+}
